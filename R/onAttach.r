ed9bfe73746d29f4a2877c8619580c3f8e1ae325
matthieu.lesnoff@gmail.com <matthieu.lesnoff@gmail.com>
2019-09-27 15:20:08 +0200

    1.0-1

---
+++ R/onAttach.r
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname){
+  
+    packageStartupMessage(
+      "Package asdmonitor version : 0.1-1 \n
+      --- A developing version of the package is available \n
+      --- at https://github.com/mlesnoff/asdmonitor \n 
+      --- You may check for more recent versions.\n" 
+      )
+
+  }
